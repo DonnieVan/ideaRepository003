2b5d2d630ce814054617f80acf56f9f3b43577b8
DonnieVan <360259440@qq.com>
2021-10-22 14:46:35 +0800

    不提交.idea

---
+++ src/Test001.java
@@ -0,0 +1,10 @@
+/**
+ * @author: 凡玉丰
+ * @date: 2021/10/22
+ * @description:
+ */
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("Test001");
+    }
+}
